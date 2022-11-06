7519c13b81c5196eb271887e87de00118384f431
NU2M <mybdye@gmail.com>
2022-11-06 16:22:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,45 @@
+#### ✏️ NOTE
+- 11.06
+  - 新建
+      * 每次运行时间约 1 分钟
+
+#### 🌟️ SUGGESTION
+```diff
+!自定义schedule
+.github/workflows/main.yml
+
+例如：
+schedule:
+    # UTC (国内 UTC+8)
+    - cron: '03 02 */2 * *'   
+    # 每2天 10:03am 执行
+    
+!规则参考
+  * * * * *
+  | | | | |
+  | | | | +----- day of week (0 - 7) (Sunday=0 or 7) OR sun,mon,tue,wed,thu,fri,sat
+  | | | +------- month (1 - 12) OR jan,feb,mar,apr ...
+  | | +--------- day of month (1 - 31)
+  | +----------- hour (0 - 23)
+  +------------- minute (0 - 59)
+```
+
+#### ㊙️ SECRET
+  |YOU SECRET NAME|YOU SECRET VALUE|
+  |-----|--|
+  |`URL_BASE`|网址，不要带有`https://` 和 `/` |
+  |`USERNAME`|你的邮箱|
+  |`MAILPARSER`|pin json download link https://files.mailparser.io/d/xxxxxxxx
+(需设置 `G**** Authentication` 邮件转发到 mailparser 并设置 pin 解析规则然后新建 json 下载链接)
+官网：https://app.mailparser.io|
+  |`BARK_TOKEN`|(可选) api.day.app/`BARK_TOKEN`/ 详见 https://github.com/Finb/Bark|
+  |`TG_BOT_TOKEN`|(可选) `xxxxxx:xxxxxxxxxxxxx`|
+  |`TG_USER_ID`|(可选) 给 bot `@userinfobot` 发送 `/start`|
+
+#### 📌 pin (别改其他名字)
+<img src="pin.png">
+
+#### 📚 THANKS
+- [SeleniumBase](https://github.com/seleniumbase)
+- [Python](https://www.python.org/)
+- [PyCharm CE](https://www.jetbrains.com/pycharm/)
---
+++ main.py
@@ -0,0 +1,169 @@
+# https://github.com/mybdye 🌟
+
+
+import os, requests
+from seleniumbase import SB
+
+
+def login():
+    print('- login')
+    try:
+        sb.open(urlLogin)
+        sb.assert_text('Login your account', 'h2', timeout=20)
+        print('- access')
+    except Exception as e:
+        print('👀 ', e, '\n try again!')
+        sb.open(urlLogin)
+        sb.assert_text('Login your account', 'h2', timeout=20)
+        print('- access')
+    #sb.switch_to_default_content()  # Exit all iframes
+    sb.sleep(1)
+    sb.type('#email', username)
+    sb.sleep(1)
+    sb.click('button:contains("send access code to email")')
+    sb.sleep(4)
+    sb.type('#mailcode', get_pin())
+    sb.click('button:contains("Login")')
+    sb.sleep(10)
+    try:
+        sb.assert_text('Notification', 'div[class="ui blue header"]')
+        print('- login success')
+        return True
+    except Exception as e:
+        print('- login issue:', e)
+        return False
+
+
+def get_pin():
+    print('- get pin')
+    response = requests.get(url=mailparser)
+    pin = response.json()[0]['pin']
+    print('- pin:', pin)
+    return pin
+
+
+def checkin():
+    global body
+    print('- checkin')
+    sb.open(urlCheckin)
+    checkIn = 'button[class="ui positive button"]'
+    sb.assert_element(checkIn)
+    print('- click checkin')
+    sb.click(checkIn)
+    sb.sleep(4)
+    userInfo = sb.get_text('div.row p')
+    #print('userInfo:', userInfo)
+    checkInfo = sb.get_text('div[class="ui icon positive message"]')
+    #print('checkInfo:', checkInfo)
+    body = checkInfo + '\n' + userInfo
+
+def screenshot():
+    global body
+    print('- screenshot')
+    sb.save_screenshot(imgFile, folder=os.getcwd())
+    print('- screenshot done')
+    sb.open_new_window()
+    print('- screenshot upload')
+    sb.open('http://imgur.com/upload')
+    sb.choose_file('input[type="file"]', os.getcwd() + '/' + imgFile)
+    sb.sleep(6)
+    imgUrl = sb.get_current_url()
+    i = 1
+    while not '/a/' in imgUrl:
+        if i > 3:
+            break
+        print('- waiting for url... *', i)
+        sb.sleep(2)
+        imgUrl = sb.get_current_url()
+        i += 1
+    print('- 📷 img url:', imgUrl)
+    body = imgUrl
+    print('- screenshot upload done')
+
+    return imgUrl
+
+
+def push(body):
+    print('- body: %s \n- waiting for push result' % body)
+    # bark push
+    if barkToken == '':
+        print('*** No BARK_KEY ***')
+    else:
+        barkurl = 'https://api.day.app/' + barkToken
+        title = urlBase
+        rq_bark = requests.get(url=f'{barkurl}/{title}/{body}?isArchive=1')
+        if rq_bark.status_code == 200:
+            print('- bark push Done!')
+        else:
+            print('*** bark push fail! ***', rq_bark.content.decode('utf-8'))
+    # tg push
+    if tgBotToken == '' or tgUserID == '':
+        print('*** No TG_BOT_TOKEN or TG_USER_ID ***')
+    else:
+        body = urlBase + '\n\n' + body
+        server = 'https://api.telegram.org'
+        tgurl = server + '/bot' + tgBotToken + '/sendMessage'
+        rq_tg = requests.post(tgurl, data={'chat_id': tgUserID, 'text': body}, headers={
+            'Content-Type': 'application/x-www-form-urlencoded'})
+        if rq_tg.status_code == 200:
+            print('- tg push Done!')
+        else:
+            print('*** tg push fail! ***', rq_tg.content.decode('utf-8'))
+    print('- finish!')
+
+
+##
+try:
+    urlBase = os.environ['URL_BASE']
+except:
+    # 本地调试用， please type here the website address without any 'https://' or '/'
+    urlBase = ''
+try:
+    username = os.environ['USERNAME']
+except:
+    # 本地调试用
+    username = ''
+try:
+    mailparser = os.environ['MAILPARSER']
+except:
+    # 本地调试用
+    mailparser = ''
+try:
+    barkToken = os.environ['BARK_TOKEN']
+except:
+    # 本地调试用
+    barkToken = ''
+try:
+    tgBotToken = os.environ['TG_BOT_TOKEN']
+except:
+    # 本地调试用
+    tgBotToken = ''
+try:
+    tgUserID = os.environ['TG_USER_ID']
+except:
+    # 本地调试用
+    tgUserID = ''
+##
+body = ''
+imgFile = urlBase + '.png'
+##
+urlLogin = 'https://' + urlBase + '/login'
+urlCheckin = 'https://' + urlBase + '/console/checkin'
+
+with SB(uc=True) as sb:  # By default, browser="chrome" if not set.
+    print('- 🚀 loading...')
+    if urlBase != '' and username != '' and mailparser != '':
+        try:
+            if login():
+                checkin()
+        except Exception as e:
+            print('💥', e)
+            try:
+                screenshot()
+            finally:
+                push(e)
+        push(body)
+    else:
+        print('- please check urlBase/username/mailparser')
+
+# END
