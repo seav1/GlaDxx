787389c63abb6262ed5640fa22d3e849636174b8
Daniel <DanielWu@duck.com>
2024-07-04 04:38:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def checkin():
             "cookie": cookies
         }
         r_checkin = requests.post(url=checkinUrl, headers=headers, data=data)
-        r_status = requests.get(url=statusUrl, headers=headers, timeout=5)
+        r_status = requests.get(url=statusUrl, headers=headers, timeout=10)
         try:
             s = 'email:%s***\nstatus:%s\ntraffic:%.2f GB\nleftDays:%s\ndetail:%s' % (r_status.json()["data"]["email"][:3], r_checkin.json()["message"], float(r_status.json()["data"]["traffic"]/1024/1024/1024), int(float(r_status.json()["data"]["leftDays"])), r_checkin.json()["list"][0]["detail"])
         except KeyError as e:
